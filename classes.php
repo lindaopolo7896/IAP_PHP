c72421839f7f7b3093da0d0be82ac80e3b1d784b
lindaopolo7896 <eleanor.opolo@strathmore.edu>
2025-09-02 22:51:34 +0300

    Added classes

---
+++ classes.php
@@ -0,0 +1,16 @@
+<?php
+
+//create a class
+ class Sample{
+  public function greet(){
+    return "Hello form Sample Class!";
+  }
+
+  public function week_day(){
+    return "Today is ". date("l");
+  }
+ }
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-print "Hello world"
+require "classes.php";
+
+ //call the class
+ //create an instance
+ $sample=new Sample();
+
+ //call the method
+echo $sample->greet();
+echo $sample->week_day();
 ?>
